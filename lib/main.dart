9a8662b4b99568f37237e52e02913092ba3b5173
Domenica-Munoz <56518381+Domenica-Munoz@users.noreply.github.com>
2023-10-12 22:58:24 -0500

    Add calendar in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 import 'main_app.dart';
 
 void main() async {
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,61 +3,143 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import 'login.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-  final Box _boxLogin = Hive.box("login");
+import '../utils.dart';
+class Home extends StatefulWidget {
+  @override
+  _TableRangeExampleState createState() => _TableRangeExampleState();
+}
+
+class _TableRangeExampleState extends State<Home> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOn; // Can be toggled on/off by longpressing a date
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+  Map<DateTime, List<Event>> events={};
+  TextEditingController _eventController = TextEditingController();
+
+  late final ValueNotifier<List<Event>> _selectedEvents;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier<List<Event>>(_getEventsForDay(DateTime.now()));
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Rebert Medicity"),
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white),
-              ),
-              child: IconButton(
-                onPressed: () {
-                  _boxLogin.clear();
-                  _boxLogin.put("loginStatus", false);
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return const Login();
-                      },
+        title: Text('Agenda'),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _eventController.clear();
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    scrollable:true,
+                    title: Text("Añadir un evento"),
+                    content: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: TextField(
+                          controller: _eventController,
+                        ),
                     ),
-                  );
+                    actions: [
+                      ElevatedButton(
+                          onPressed: (){
+                            if (_selectedDay != null) {
+                              List<Event> listaAux = _getEventsForDay(_selectedDay!);
+                              listaAux.add(Event(_eventController.text));
+                              events[_selectedDay!] = listaAux;
+                              _selectedEvents.value = listaAux;
+                            }else{
+                              events.addAll({
+                                _selectedDay!: [Event(_eventController.text)]
+                              });
+                              _selectedEvents.value=_getEventsForDay(_selectedDay!);
+                            }
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Agregar")
+                      )
+                    ],
+                );
+              });
+      },child: Icon(Icons.add)),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
+            calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
+            eventLoader: _getEventsForDay,
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  _rangeStart = null; // Important to clean those
+                  _rangeEnd = null;
+                  _rangeSelectionMode = RangeSelectionMode.toggledOff;
+                  _selectedEvents.value = _getEventsForDay(selectedDay);
+                });
+              }
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                valueListenable: _selectedEvents,
+                builder: (context, value, _){
+                  return ListView.builder(itemCount: value.length,itemBuilder: (context, index){
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(onTap: ()=> print(""), title: Text('${value[index]}'),),
+                    );
+                  });
                 },
-                icon: const Icon(Icons.logout_rounded),
-              ),
-            ),
+              )
           )
         ],
-      ),
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Bienvenido/a 🎉",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _boxLogin.get("userName"),
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ],
-        ),
-      ),
+      )
     );
+  }
+
+  List<Event> _getEventsForDay(DateTime day){
+    return events[day]??[];
   }
 }
